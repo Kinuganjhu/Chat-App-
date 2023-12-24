2eee8e69de0669775377355b0ac3b6521d915635
Kinuganjhu <axelover179@gmail.com>
2023-12-24 16:36:28 +0530

    remove unnecessary packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css';
+import {Button,Progress,Input} from 'rsuite';
 function App() {
   return (
-    <div>
-      <header> 
-      hello world my name is kinu ganjhu
-      </header>
+    <div className ='text-center'>
+    <h1>ahello world</h1>
+    <Input/>
+    <Button color ='red'>Hello World</Button>
+      
+  <Progress.Line vertical percent = {80}/>
+
     </div>
+    
   );
 }
 
