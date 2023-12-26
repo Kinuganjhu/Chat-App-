b4f05068c89b87c0c974f6b0482f3784b2936c85
Kinuganjhu <axelover179@gmail.com>
2023-12-26 05:38:07 +0530

    initialize firebase app

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: "AIzaSyAzTR7k3xizJdyqW0DkBksxdJAW5FqtcBw",
+  authDomain: "chat-web-app-49f76.firebaseapp.com",
+  databaseURL: "https://chat-web-app-49f76-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-web-app-49f76",
+  storageBucket: "chat-web-app-49f76.appspot.com",
+  messagingSenderId: "821349516173",
+  appId: "1:821349516173:web:6d26e9a2742a8e7b45b3c4"
+  
+  
+}
+const app = firebase.initializeApp(config);
