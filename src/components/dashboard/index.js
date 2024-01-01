4cd057cc9d9c40573e030604cc9880c4ba61fbc4
Kinuganjhu <axelover179@gmail.com>
2024-01-01 23:19:50 +0530

    added dashboard edit option

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Drawer, Divider } from 'rsuite';
 import PropTypes from 'prop-types'; // Import PropTypes
 import { useProfile } from '../../context/profile.context';
-
+import EditableInput from '../EditableInput';
+const onSave = (newData)=>{
+  
+}
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
   return (
@@ -14,6 +17,12 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+      
+        <EditableInput
+        name ='Nickname' initialValue ={profile.name} onSave ={onSave}
+        
+        label ={<h6 className ="mb-2">Nickname</h6>}/>
       </Drawer.Body>
       <Drawer.Footer>
         {/* Ensure the onSignOut function is used */}
